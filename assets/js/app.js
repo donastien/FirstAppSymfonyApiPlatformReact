3c4868d00667b718f90f4dbcb654a00844d25afa
donastien <donastien.karoum@gmail.com>
2020-09-10 21:52:49 +0200

    Installation de React et gestion des Customers et Invoices

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import CustomersPage from './pages/CustomersPage';
+import InvoicesPage from './pages/InvoicesPage';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
@@ -10,7 +15,19 @@ import '../css/app.css';
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
 const App = () => {
-  return <h1>Bonjour à tous</h1>;
+  return (
+    <HashRouter>
+      <Navbar />
+
+      <main className='container pt-5'>
+        <Switch>
+          <Route path='/invoices' component={InvoicesPage} />
+          <Route path='/customers' component={CustomersPage} />
+          <Route path='/' component={HomePage} />
+        </Switch>
+      </main>
+    </HashRouter>
+  );
 };
 
 const rootElement = document.querySelector('#app');
--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -28,7 +28,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *          "description"="Incrémente le chrono d'une facture donnée"
  * }}},
  * attributes={
- *      "pagination_enabled"=true,
+ *      "pagination_enabled"=false,
  *      "pagination_items_per_page"=20,
  *      "order": {"sentAt": "desc"}
  *

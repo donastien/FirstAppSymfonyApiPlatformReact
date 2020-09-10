7d6028d47219a688d34bac2c823acafe241e364c
donastien <donastien.karoum@gmail.com>
2020-09-11 01:22:16 +0200

    Mise en place de l'authentification

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import {
+  HashRouter,
+  Switch,
+  Route,
+  withRouter,
+  Redirect,
+} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
+import AuthAPI from './services/authAPI';
+import AuthContext from './contexts/AuthContext';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 import CustomersPage from './pages/CustomersPage';
 import InvoicesPage from './pages/InvoicesPage';
+import LoginPage from './pages/LoginPage';
+import PrivateRoute from './components/PrivateRoute';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+AuthAPI.setup();
 
 const App = () => {
-  return (
-    <HashRouter>
-      <Navbar />
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    AuthAPI.isAuthenticated()
+  );
 
-      <main className='container pt-5'>
-        <Switch>
-          <Route path='/invoices' component={InvoicesPage} />
-          <Route path='/customers' component={CustomersPage} />
-          <Route path='/' component={HomePage} />
-        </Switch>
-      </main>
-    </HashRouter>
+  const NavbarWithRouter = withRouter(Navbar);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      <HashRouter>
+        <NavbarWithRouter />
+
+        <main className='container pt-5'>
+          <Switch>
+            <Route path='/login' component={LoginPage} />
+            <PrivateRoute path='/invoices' component={InvoicesPage} />
+            <PrivateRoute path='/customers' component={CustomersPage} />
+            <Route path='/' component={HomePage} />
+          </Switch>
+        </main>
+      </HashRouter>
+    </AuthContext.Provider>
   );
 };
 
